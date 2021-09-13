97868732f2e4152b6062428eb016a67bc44b135b
durante.elena00 <durante.elena00@gmail.com>
2021-09-14 00:53:24 +0200

    coomplete the assignmente

---
+++ src/components/Search.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <input
+      type="text"
+      placeholder="Inserisci film o serie tv..."
+      v-model.trim="inputValue"
+      @keyup.enter="emitSearch"
+    />
+    <button type="submit" @click="emitSearch">
+      <i class="fas fa-search"></i>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Search",
+  data() {
+    return {
+      inputValue: "",
+    };
+  },
+  methods: {
+    emitSearch() {
+      this.$emit("search", this.inputValue);
+    },
+  },
+};
+</script>
+
+<style scoped>
+</style>
